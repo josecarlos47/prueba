f1e798f8bc7130b320836728382b6c93bc95c10f
josecarlos47 <114216990+josecarlos47@users.noreply.github.com>
2023-11-14 14:00:36 +0100

    Update holaMundi.java

--- holaMundi.java
+++ holaMundi.java
@@ -1,8 +1,8 @@
 public class holaMundi{
     public static void main (String [] args){
-    System.out.println("*".repeat(10));
+    System.out.println("*".repeat(14));
     System.out.println("Hola mundo de visual");
-    System.out.println("*".repeat(10));
+    System.out.println("*".repeat(14));
     
     }
 }
